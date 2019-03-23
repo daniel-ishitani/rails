8db751c1c86bbc17a87fa2e2df6ff61e48d7d3d5
Daniel <ishitanidim@gmail.com>
2019-03-23 00:24:07 +0000

    user, articles association and debugger

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_013715) do
+ActiveRecord::Schema.define(version: 2019_03_23_000501) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
