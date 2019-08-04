faef44de4bdeb4cd960e6ac1b1dc9461dca156d8
daniel-ishitani <ishitanidim@gmail.com>
2019-08-04 23:58:28 +0000

    category and tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,39 @@
+class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
+
+    def index
+        @categories = Category.paginate(page: params[:page], per_page: 3)
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = 'Category created successfully'
+            redirect_to categories_path
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        
+    end
+
+    private
+
+    def require_admin
+        if !logged_in? || (logged_in? and !current_user.admin?)
+            flash[:danger] = 'User not allowed to perform the action.'
+            redirect_to categories_path
+        end
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+end
